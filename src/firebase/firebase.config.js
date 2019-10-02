3848304460bd0874d2d9cf0a76d856ec83838950
creyente1897 <ayushsrivastava1897@gmail.com>
2019-10-03 00:27:05 +0530

    Updated config files

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,11 +1,11 @@
 const FIREBASE_CONFIG = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    databaseURL: process.env.DATABASE_URL,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID
+    apiKey: "AIzaSyBcRIwwkb1Pz-6EKJ7snd9M35EhBHf248w",
+    authDomain: "dapperdb-ee83a.firebaseapp.com",
+    databaseURL: "https://dapperdb-ee83a.firebaseio.com",
+    projectId: "dapperdb-ee83a",
+    storageBucket: "dapperdb-ee83a.appspot.com",
+    messagingSenderId: "294079479462",
+    appId: "1:294079479462:web:b235c7a0259e003fbdbe67"
 };
 
 export default FIREBASE_CONFIG;
