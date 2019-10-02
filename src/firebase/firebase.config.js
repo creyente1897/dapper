dbf5542cd3ae2059d02134e898a557c7ccfdb97a
creyente1897 <ayushsrivastava1897@gmail.com>
2019-10-03 00:04:05 +0530

    Added firebase config file with heroku environment variables

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const FIREBASE_CONFIG = {
+    apiKey: process.env.APP_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID
+};
+
+export default FIREBASE_CONFIG;
